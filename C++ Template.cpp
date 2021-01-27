5158cb1322eff8aa092c8311aea3b691cc5a1d6d
albertolg101 <67377837+albertolg101@users.noreply.github.com>
2021-01-26 22:35:10 -0500

    Update C++ Template.cpp

--- C++ Template.cpp
+++ C++ Template.cpp
@@ -3,30 +3,23 @@
 using namespace std;
 using ll = long long;
 
-#ifdef __debug_leywar
+#ifdef __local_leywar
 #include <debug/debugger.h>
-#elseif ALBE_PC
-#include "debug/debugger.h>
 #endif
 
 #define endl '\n'
 
 const int INF = 2e9,
-          INFLL = 1e18,
           MOD = 1e9+7;
+
+const ll INFLL = 1e18;
 
 int32_t main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-#ifdef ALBE_PC
-    freopen("z.txt", "r", stdin);
-#endif // ALBE_PC
-
-
-          
-          
+    
 
     return 0;
 }
