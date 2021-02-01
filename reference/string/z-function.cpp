d189ff8b7af1b37781d6bbbce5825c210f1d927b
albertolg101 <67377837+albertolg101@users.noreply.github.com>
2021-02-01 02:15:53 -0500

    Create z-function.cpp

---
+++ reference/string/z-function.cpp
@@ -0,0 +1,20 @@
+//From cp-algorithm (https://cp-algorithms.com/string/z-function.html)
+//Not tested yet
+
+vector<int> z_function (string &s)
+{
+	vector<int> z(s.size());
+	for(int i = 1, l = 0, r = 0 ; i < s.size() ; i++)
+	{
+		if(i <= r)
+			z[i] = min(r - i + 1, z[i - l]);
+
+		while(i + z[i] < s.size() and s[z[i]] == s[i + z[i]])
+			z[i]++;
+
+		if(i + z[i] - 1 > r)
+			l = i, r = i + z[i] - 1;
+	}
+
+	return z;
+}
